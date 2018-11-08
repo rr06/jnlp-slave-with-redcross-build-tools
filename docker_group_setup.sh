6b01ef3d007f2b2dcb9ceae0d0fcbb8e92b62aec
Rob Romito <rob@robertromito.com>
2018-11-08 11:31:05 -0500

    WIP

--- docker_group_setup.sh
+++ docker_group_setup.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 sudo usermod -a -G $(cat /etc/group_host | grep docker | cut -d':' -f3) jenkins
 echo "Running Jenkins build agent"
-/opt/bin/entry_point.sh /usr/local/bin/jenkins-slave
+/opt/bin/entry_point.sh /usr/local/bin/jenkins-slave "$@"
